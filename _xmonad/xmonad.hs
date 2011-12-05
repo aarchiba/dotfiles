79dc96b28115f7fd3f6f64dd78412358473f29f6
John Anderson <sontek@gmail.com>
2011-12-05 17:42:42 -0500

    Pick better layouts

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -5,6 +5,8 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeysP)
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Spiral
+import XMonad.Layout.Tabbed
 import System.IO
 
 myTerminal = "xfce4-terminal"
@@ -12,13 +14,18 @@ myManageHook = composeAll
     [ className =? "Gimp"      --> doFloat
     , className =? "Vncviewer" --> doFloat
     ]
+myLayouts = (smartBorders $ avoidStruts $
+                        Full
+                        ||| spiral (8/10) 
+                        ||| Tall 1 (3/100) (10/12) 
+                        )
 
 
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/peridot/.xmobarrc"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
-        , layoutHook = smartBorders $ avoidStruts  $  layoutHook defaultConfig
+        , layoutHook = myLayouts
         , logHook = dynamicLogWithPP xmobarPP
             { ppOutput = hPutStrLn xmproc
             , ppTitle = xmobarColor "green" "" . shorten 50
