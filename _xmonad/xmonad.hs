0ef675d388f9e93f21c1b33478e4f2f884ffe132
John Anderson <sontek@gmail.com>
2011-12-05 16:15:36 -0500

    xmonad setup

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -1,6 +1,35 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import qualified XMonad.StackSet as W
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeysP)
+import XMonad.Layout.NoBorders
+import System.IO
 
-main = xmonad defaultConfig
-    { modMask = mod4Mask -- user Super instead of Alt
-    , terminal = "urxvt"
-    }
+myTerminal = "xfce4-terminal"
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+    , className =? "Vncviewer" --> doFloat
+    ]
+
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/peridot/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
+        , layoutHook = smartBorders $ avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            }
+        , terminal = myTerminal
+        } `additionalKeysP`
+        [ ("<Print>", spawn "scrot")
+        , ("<XF86AudioLowerVolume>", spawn "amixer set Master 1- unmute")
+        , ("<XF86AudioRaiseVolume>", spawn "amixer set Master 1+ unmute")
+        , ("<XF86Calculator>", windows $ W.greedyView "1")
+        , ("<XF86HomePage>", windows $ W.greedyView "2")
+        , ("<XF86Mail>", windows $ W.greedyView "3")
+        , ("M4-l", spawn "xscreensaver-command -lock")
+        ]
