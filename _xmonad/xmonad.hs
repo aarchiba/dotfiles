199a3e2da81e8850ee1898a64b48d2b675a73288
John Anderson <sontek@gmail.com>
2011-12-11 13:53:02 -0500

    Fixed fullscreen and fractional lines in terminals

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -2,40 +2,39 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import qualified XMonad.StackSet as W
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeysP)
 import XMonad.Util.Scratchpad
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Spiral
-import XMonad.Layout.Tabbed
+import XMonad.Layout.LayoutHints
+import XMonad.Config.Xfce
 import XMonad.Actions.WindowBringer
 import System.IO
 
-myTerminal = "urxvt"
+myTerminal = "xfce4-terminal"
 myManageHook = composeAll
     [ className =? "Gimp"      --> doFloat
     , className =? "Vncviewer" --> doFloat
+    , isFullscreen --> doFullFloat
     ]
-myLayouts = (smartBorders $ avoidStruts $
+myLayouts = (layoutHints $ smartBorders $ avoidStruts $
                         Full
                         ||| spiral (8/10) 
                         ||| Tall 1 (3/100) (10/12) 
                         )
 
-
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/peridot/.xmobarrc"
-    xmonad $ defaultConfig
+    xmonad $ xfceConfig
         { manageHook = scratchpadManageHookDefault 
                    <+> manageDocks 
                    <+> myManageHook 
                    <+> manageHook defaultConfig
         , layoutHook = myLayouts
-        , logHook = dynamicLogWithPP xmobarPP
-            { ppOutput = hPutStrLn xmproc
-            , ppTitle = xmobarColor "green" "" . shorten 50
-            }
         , terminal = myTerminal
+        , modMask = mod4Mask
         } `additionalKeysP`
         [ ("<Print>", spawn "scrot")
         , ("<XF86AudioLowerVolume>", spawn "amixer set Master 1- unmute")
@@ -47,4 +46,5 @@ main = do
         , ("M-g", gotoMenu)
         , ("M-b", bringMenu)
         , ("M4-l", spawn "xscreensaver-command -lock")
+        , ("M-Q", spawn "xfce4-session-logout")
         ]
